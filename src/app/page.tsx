476125f1b92fff2a9d4c89e59111cc275cd9929d
LN <leonelngoya@gmail.com>
2024-05-11 14:43:25 +0100

    Refactor font import and update body class in layout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className=""></main>
+    <main className="relative w-full h-screen flex items-center justify-center p-12">
+      <CopyRight />
+    </main>
   );
+}
+
+export const CopyRight = () => {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 flex items-center gap-4 p-4 border-t bg-white z-20">
+      <a href="https://lndev.me" className="underline underline-offset-2">Code by LN</a>
+      <a href="https://github.com/ln-dev7/grid-transition-media" className="underline underline-offset-2">GitHub</a>
+    </div>
+  )
 }
