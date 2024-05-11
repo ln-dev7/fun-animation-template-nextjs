b602d74965a1e0ce4667acc6c29868628798b94b
LN <leonelngoya@gmail.com>
2024-05-11 15:16:50 +0100

    Refactor CopyRight component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
   );
 }
 
-export const CopyRight = () => {
+function CopyRight() {
   return (
     <div className="absolute bottom-0 left-0 right-0 flex items-center gap-4 p-4 border-t bg-white z-20">
       <a href="https://lndev.me" className="underline underline-offset-2">Code by LN</a>
-      <a href="https://github.com/ln-dev7/grid-transition-media" className="underline underline-offset-2">GitHub</a>
+      <a href="https://github.com/ln-dev7/" className="underline underline-offset-2">GitHub</a>
     </div>
   )
 }
