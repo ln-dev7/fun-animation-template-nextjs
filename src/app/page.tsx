5db3f0498d1d4ff98a44dac1386f01c41de32927
LN <leonelngoya@gmail.com>
2024-06-25 09:31:18 +0100

    few updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 function CopyRight() {
   return (
     <div className="absolute bottom-0 left-0 right-0 flex items-center gap-4 p-4 border-t bg-white z-20">
-      <a href="https://lndev.me" className="underline underline-offset-2">Code by LN</a>
+      <a href="https://ui.lndev.me" className="underline underline-offset-2">Code by LN</a>
       <a href="https://github.com/ln-dev7/" className="underline underline-offset-2">GitHub</a>
     </div>
   )
